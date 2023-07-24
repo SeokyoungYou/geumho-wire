b820d1a548bcde827202172b706738dc0c7a535f
Seokyoung <sk.yu@autopediacar.com>
2023-07-24 16:41:08 +0900

    style: apply eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { openGraphImage } from "./shared-metadata";
+
 import RootStyleRegistry from "./emotion";
+import { openGraphImage } from "./shared-metadata";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode; }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
