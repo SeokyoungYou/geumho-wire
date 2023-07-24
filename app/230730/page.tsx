6cde804dd4a1ff3ce5bc690f8b3e804463a631ca
Seokyoung <sk.yu@autopediacar.com>
2023-07-24 16:22:24 +0900

    chore: eslint setup

--- app/230730/page.tsx
+++ app/230730/page.tsx
@@ -11,11 +11,16 @@ import {
   Title,
   Image,
   Drawer,
-  MultiSelect
+  MultiSelect,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconInfoCircle,  IconMenu2, IconPhotoSearch } from "@tabler/icons-react";
-import { useEffect, useRef, useState } from "react";
+import {
+  IconInfoCircle,
+  IconMenu2,
+  IconPhotoSearch,
+} from "@tabler/icons-react";
+import React, { useEffect, useState } from "react";
+
 import Competition from "./Competition";
 import en from "./en.json";
 import ko from "./ko.json";
@@ -39,55 +44,63 @@ export default function Page() {
 
   const [locale, setLocale] = useState(Locale.ko);
   const t = locale === Locale.ko ? ko : en;
-  const [names, setNames] = useState<string[]>(t.competitors.map(c=> c.name));
+  const [names, setNames] = useState<string[]>(
+    t.competitors.map((c) => c.name),
+  );
   const [filteredNames, setFilteredNames] = useState<string[]>(names);
 
-  const [burgerOpened, { close : burgerClose, open : burgerOpen }] = useDisclosure(false);
+  const [burgerOpened, { close: burgerClose, open: burgerOpen }] =
+    useDisclosure(false);
 
-
-  useEffect(()=> {
-    setNames(t.competitors.map(c=> c.name));
-    setFilteredNames(t.competitors.map(c=> c.name));
-  }, [locale])
+  useEffect(() => {
+    setNames(t.competitors.map((c) => c.name));
+    setFilteredNames(t.competitors.map((c) => c.name));
+  }, [t.competitors]);
 
   const rows = t.competitors.map((element) => (
-    <>
+    <React.Fragment key={element.name}>
       {filteredNames.includes(element.name) && (
-        <tr key={element.name}>
+        <tr>
           <td>{element.time}</td>
           <td>{element.name}</td>
           <td>{element.mat}</td>
           <td>
             <Button
               variant="light"
-              leftIcon={<IconPhotoSearch size="1rem"/>} size="xs"
+              leftIcon={<IconPhotoSearch size="1rem" />}
+              size="xs"
               onClick={() => {
                 setClickedPerson(element);
                 open();
               }}
-              >
+            >
               {element.division}
             </Button>
           </td>
         </tr>
       )}
-    </>
+    </React.Fragment>
   ));
 
-
-
   return (
-    <main className="relative flex flex-col min-h-screen gap-4 p-2 mt-14">
-      <Drawer opened={burgerOpened} onClose={burgerClose} title="Filter" size="xs" position="right" transitionProps={{ transition: 'slide-left' }}>
+    <main className="relative mt-14 flex min-h-screen flex-col gap-4 p-2">
+      <Drawer
+        opened={burgerOpened}
+        onClose={burgerClose}
+        title="Filter"
+        size="xs"
+        position="right"
+        transitionProps={{ transition: "slide-left" }}
+      >
         <MultiSelect
-          onChange={(e)=> setFilteredNames(e)}
+          onChange={(e) => setFilteredNames(e)}
           data={names}
           label={t.table.name}
           placeholder="Select Names"
           defaultValue={filteredNames}
         />
       </Drawer>
-      <section className="fixed top-0 flex items-center self-center justify-between w-full p-3 bg-slate-100">
+      <section className="fixed top-0 flex w-full items-center justify-between self-center bg-slate-100 p-3">
         <Switch
           labelPosition="left"
           size="md"
@@ -99,7 +112,7 @@ export default function Page() {
           }}
         />
         <ActionIcon onClick={burgerOpen}>
-          <IconMenu2 size="2rem"/>
+          <IconMenu2 size="2rem" />
         </ActionIcon>
       </section>
       <section>
@@ -139,11 +152,16 @@ export default function Page() {
             title={`${clickedPerson.name}`}
             centered
           >
-            <Image maw={330} mx="auto" radius="md"  src={clickedPerson.leagueTable} alt="대진표이미지" />
+            <Image
+              maw={330}
+              mx="auto"
+              radius="md"
+              src={clickedPerson.leagueTable}
+              alt="대진표이미지"
+            />
           </Modal>
         )}
       </section>
-
     </main>
   );
 }
