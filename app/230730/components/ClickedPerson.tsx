97ddb56a611537bcad4610971b413f064430c2f2
Seokyoung <sk.yu@autopediacar.com>
2023-07-25 10:31:28 +0900

    feat: implement next/image placeholder and width

--- app/230730/components/ClickedPerson.tsx
+++ app/230730/components/ClickedPerson.tsx
@@ -1,4 +1,5 @@
-import { Image, Modal } from "@mantine/core";
+import { Modal } from "@mantine/core";
+import Image from "next/image";
 
 import { Person } from "../page";
 
@@ -22,14 +23,20 @@ const ClickedPerson: React.FC<ClickedPerson> = ({
       title={`${clickedPerson.name}`}
       centered
     >
-      <Image
-        maw={330}
-        mx="auto"
-        radius="md"
-        src={clickedPerson.leagueTable}
-        alt="대진표이미지"
-        withPlaceholder
-      />
+      <div className="relative flex w-full items-center justify-center">
+        <Image
+          // width={300}
+          // height={300}
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="h-auto w-full"
+          src={clickedPerson.leagueTable}
+          alt="대진표이미지"
+          placeholder="blur"
+          blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAZAAAABkCAYAAACoy2Z3AAABPElEQVR42u3VMQEAMAjAsKF8qlAAwtDAwZdI6NPI6v8AYCkMBAADAcBAADAQAAwEAAwEAAMBwEAAMBAADAQADAQAAwHAQAAwEAAMBAAMBAADAcBAADAQAAwEAAwEAAMBwEAAMBAADAQADAQAAwHAQAAwEAAMBAAMBAADAcBAADAQAAxEBgAMBAADAcBAADAQADAQAAwEAAMBwEAAMBAAMBAADAQAAwHAQAAwEAAwEAAMBAADAcBAADAQADAQAAwEAAMBwEAAMBAAMBAADAQAAwHAQAAwEAAwEAAMBAADAcBAADAQADAQAAwEAAMBwEAAMBADAcBAADAQAAwEAAMBAAMBwEAAMBAADAQAAwEAAwHAQAAwEAAMBAADAQADAcBAADAQAAwEAAMBAAMBwEAAMBAADAQAAwEAAwHgxgBR9xGAaA0DAgAAAABJRU5ErkJggg=="
+        />
+      </div>
     </Modal>
   );
 };
